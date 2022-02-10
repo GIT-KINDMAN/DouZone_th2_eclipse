8cdf92534360aca75c9a88057434d0cf2e27377d
GIT-KINDMAN <95545623+GIT-KINDMAN@users.noreply.github.com>
2022-02-10 17:40:41 +0900

    22.02.10

---
+++ day10_collection/src/ex07/textPackage/Desending.java
@@ -0,0 +1,19 @@
+package ex07.textPackage;
+
+import java.util.Comparator;
+
+public class Desending implements Comparator {
+
+	@Override
+	public int compare(Object o1, Object o2) {
+		if( o1 instanceof Comparable && o2  instanceof Comparable ) {
+			Comparable c1 = (Comparable)o1;
+			Comparable c2 = (Comparable)o2;
+			
+			 return  c1.compareTo(c2) * -1 ; // -1�� ���ؼ� �⺻ ���� ����� ������ ��� 
+			//return  c2.compareTo(c1);  // c1.compareTo(c2)�� ���� ��Ȳ
+		}
+		return -1 ;
+	}
+
+}
